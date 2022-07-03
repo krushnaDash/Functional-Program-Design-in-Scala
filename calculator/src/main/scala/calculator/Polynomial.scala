30fe3666bf825c92cf9126e64830859272303892
K0D03GD <krushnachandra.dash@walmart.com>
2022-07-03 11:17:15 +0530

    week 5 added

---
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -0,0 +1,21 @@
+package calculator
+
+object Polynomial extends PolynomialInterface:
+  def computeDelta(a: Signal[Double], b: Signal[Double],
+      c: Signal[Double]): Signal[Double] =   Signal(b() * b() - 4 * a() * c())
+
+  def computeSolutions(a: Signal[Double], b: Signal[Double],
+      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] =
+    Signal {
+      val d = delta()
+      if (d < 0) Set()
+      else {
+        val bVal = b()
+        val aVal = a()
+
+        Set(
+          (-bVal + Math.sqrt(d)) / (2 * aVal),
+          (-bVal - Math.sqrt(d)) / (2 * aVal)
+        )
+      }
+    }
